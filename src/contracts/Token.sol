ba9eee022fec07a2f29ad04bca205337800687b4
Fabien Mauro <mauro.fabien@gmail.com>
2021-08-03 15:02:59 +0200

    Adding transfer function with tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,15 +1,40 @@
-pragma solidity ^0.5.16;
+// SPDX-License-Identifier: <SPDX-License>
+
+pragma solidity ^0.8.0;
+
+import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 
 /**
  * The FMauro Token contract
  */
 contract Token {
+	using SafeMath for uint;
+
+	// Variables
 	string public name = "FMauro Token";
 	string public symbol = "FMA";
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
+	mapping (address => uint256) public balanceOf;
 
-  constructor() public {
+	// Events
+	event Transfer(address indexed from, address indexed to, uint256 value);
+
+  constructor() {
     totalSupply = 1000000 * (10 ** decimals);
+    balanceOf[msg.sender] = totalSupply;
   }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+  	require (_to != address(0));
+  	require (balanceOf[msg.sender] >= _value);
+  	
+  	balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+  	balanceOf[_to] = balanceOf[_to].add(_value);
+  	emit Transfer(msg.sender, _to, _value);
+  	
+  	return true;
+  }
+
 }
+ 
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,23 +1,23 @@
+import { tokens, evm_revert } from './helpers.js'
 const Token = artifacts.require("./Token");
 
 require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('Token', (accounts) => {
+contract('Token', ([deployer, receiver]) => {
+  const name = 'FMauro Token'
+  const symbol = 'FMA'
+  const decimals = '18'
+  const totalSupply = tokens(1000000)
+  let token
+  let result
+
+  beforeEach(async () => {
+    token = await Token.new()
+  })
 
   describe('deployment', () => {
-    const name = 'FMauro Token'
-    const symbol = 'FMA'
-    const decimals = '18'
-    const totalSupply = '1000000000000000000000000'
-    let token
-    let result
-
-    beforeEach(async () => {
-      token = await Token.new()
-    })
-    
     it('tracks the name', async () => {
       result = await token.name()
       result.should.equal(name)
@@ -35,8 +35,56 @@ contract('Token', (accounts) => {
 
     it('tracks the total supply', async () => {
       result = await token.totalSupply()
-      result.toString().should.equal(totalSupply)
+      result.toString().should.equal(totalSupply.toString())
     })
 
+    it('assigns the total supply to the deployer', async () => {
+      result = await token.balanceOf(deployer)
+      result.toString().should.equal(totalSupply.toString())
+    })
+  })
+
+  describe('sending tokens', () => {
+    let amount
+    let result
+    let event
+    let logs
+
+    describe('success', async () => {
+      beforeEach(async () => {
+        amount = tokens(100)
+        result = await token.transfer(receiver, amount, {from: deployer})
+      })
+
+      it('transfer token balances', async () => {
+        result = await token.balanceOf(deployer)
+        result.toString().should.equal((tokens(999900)).toString(), 'balance of deployer')
+        result = await token.balanceOf(receiver)
+        result.toString().should.equal((tokens(100)).toString(), 'balance of receiver')
+      })
+
+      it('emits a Transfer event', async () => {
+        event = result.logs[0].event
+        logs = result.logs[0].args
+        event.should.equal('Transfer')
+        logs.from.toString().should.equal(deployer, 'from of Transfer event')
+        logs.to.toString().should.equal(receiver, 'to of Transfer event')
+        logs.value.toString().should.equal(amount.toString(), 'value of Transfer event')
+      })
+    })
+
+    describe('failure', async () => {
+      it('reject insufficient balances', async () => {
+        let invalidAmount
+        invalidAmount = tokens(10000000) // 10 million - greater than total supply
+        await token.transfer(receiver, invalidAmount, { from: deployer }).should.be.rejectedWith(evm_revert);
+        invalidAmount = tokens(10) // recipient has no tokens
+        await token.transfer(deployer, invalidAmount, { from: receiver }).should.be.rejectedWith(evm_revert);
+      })
+
+      it('reject invalid recipients', async () => {
+        await token.transfer(0x0, amount, { from: deployer }).should.be.rejected;
+      })
+    })
   })
 })
